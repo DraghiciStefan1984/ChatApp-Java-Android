3db5e913f5da0802bdca73dfdf39851fae5e40b2
StefanDraghici1984 <draghicistefan1984@yahoo.com>
2018-09-17 10:06:19 +0300

    done

--- ChatApp/app/src/main/java/com/stefandraghici/chatapp/RegisterActivity.java
+++ ChatApp/app/src/main/java/com/stefandraghici/chatapp/RegisterActivity.java
@@ -1,8 +1,10 @@
 package com.stefandraghici.chatapp;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -10,20 +12,24 @@ import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.signin.internal.AuthAccountResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RegisterActivity extends AppCompatActivity
 {
     // Constants
     public static final String CHAT_PREFS = "ChatPrefs";
     public static final String DISPLAY_NAME_KEY = "username";
 
-    // TODO: Add member variables here:
-    // UI references.
-    private AutoCompleteTextView mEmailView;
-    private AutoCompleteTextView mUsernameView;
-    private EditText mPasswordView;
-    private EditText mConfirmPasswordView;
+    private AutoCompleteTextView emailView;
+    private AutoCompleteTextView usernameView;
+    private EditText passwordView;
+    private EditText confirmPasswordView;
 
-    // Firebase instance variables
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -31,13 +37,13 @@ public class RegisterActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.register_email);
-        mPasswordView = (EditText) findViewById(R.id.register_password);
-        mConfirmPasswordView = (EditText) findViewById(R.id.register_confirm_password);
-        mUsernameView = (AutoCompleteTextView) findViewById(R.id.register_username);
+        emailView = (AutoCompleteTextView) findViewById(R.id.register_email);
+        passwordView = (EditText) findViewById(R.id.register_password);
+        confirmPasswordView = (EditText) findViewById(R.id.register_confirm_password);
+        usernameView = (AutoCompleteTextView) findViewById(R.id.register_username);
 
         // Keyboard sign in action
-        mConfirmPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener()
+        confirmPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener()
         {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent)
@@ -51,7 +57,7 @@ public class RegisterActivity extends AppCompatActivity
             }
         });
 
-        // TODO: Get hold of an instance of FirebaseAuth
+        firebaseAuth=FirebaseAuth.getInstance();
     }
 
     // Executed when Sign Up button is pressed.
@@ -63,12 +69,12 @@ public class RegisterActivity extends AppCompatActivity
     private void attemptRegistration()
     {
         // Reset errors displayed in the form.
-        mEmailView.setError(null);
-        mPasswordView.setError(null);
+        emailView.setError(null);
+        passwordView.setError(null);
 
         // Store values at the time of the login attempt.
-        String email = mEmailView.getText().toString();
-        String password = mPasswordView.getText().toString();
+        String email = emailView.getText().toString();
+        String password = passwordView.getText().toString();
 
         boolean cancel = false;
         View focusView = null;
@@ -76,52 +82,66 @@ public class RegisterActivity extends AppCompatActivity
         // Check for a valid password, if the user entered one.
         if (TextUtils.isEmpty(password) || !isPasswordValid(password))
         {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
-            focusView = mPasswordView;
+            passwordView.setError(getString(R.string.error_invalid_password));
+            focusView = passwordView;
             cancel = true;
         }
 
         // Check for a valid email address.
         if (TextUtils.isEmpty(email))
         {
-            mEmailView.setError(getString(R.string.error_field_required));
-            focusView = mEmailView;
+            emailView.setError(getString(R.string.error_field_required));
+            focusView = emailView;
             cancel = true;
         }
         else if (!isEmailValid(email))
         {
-            mEmailView.setError(getString(R.string.error_invalid_email));
-            focusView = mEmailView;
+            emailView.setError(getString(R.string.error_invalid_email));
+            focusView = emailView;
             cancel = true;
         }
 
         if (cancel)
         {
-            // There was an error; don't attempt login and focus the first
-            // form field with an error.
             focusView.requestFocus();
         }
         else
-         {
-            // TODO: Call create FirebaseUser() here
-
+        {
+            createFirebaseUser();
         }
     }
 
     private boolean isEmailValid(String email)
     {
-        // You can add more checking logic here.
         return email.contains("@");
     }
 
     private boolean isPasswordValid(String password)
     {
-        //TODO: Add own logic to check for a valid password (minimum 6 characters)
-        return true;
+        String confirmedPassword=confirmPasswordView.getText().toString();
+        return confirmedPassword.equals(password) && password.length()>4;
     }
 
     // TODO: Create a Firebase user
+    private void createFirebaseUser()
+    {
+        String email=emailView.getText().toString();
+        String password=passwordView.getText().toString();
 
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
+        {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task)
+            {
+                Log.d("Chatapp", "create user; "+task.isSuccessful());
+
+                if(!task.isSuccessful())
+                {
+                    Log.d("Chatapp", "create user failed; ");
+                }
+            }
+        });
+    }
 
     // TODO: Save the display name to Shared Preferences
 
